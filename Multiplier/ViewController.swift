c1b5384e4a06bf5661c5aa40bd87d5d001693d60
mur7ay <mur7ay@me.com>
2015-11-13 14:59:36 -0500

    Added files for the multiplier app.

--- Multiplier/ViewController.swift
+++ Multiplier/ViewController.swift
@@ -9,17 +9,84 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Properties
+    var userInput = 0
+    var runningSum = 0
+    let MAXLIMIT = 100
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // Outlets
+    @IBOutlet var multipleLogo: UIImageView!
+    @IBOutlet var pressToAdd: UILabel!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var addButton: UIButton!
+    @IBOutlet var playButton: UIButton!
+    
+    
+    @IBAction func playButton(sender: UIButton) {
+        
+        if textField.text != nil && textField.text != "" {
+            
+            multipleLogo.hidden = true
+            playButton.hidden = true
+            textField.hidden = true
+            addButton.hidden = false
+            pressToAdd.hidden = false
+            
+            resetLbl()
+            userInput = Int(textField.text!)!
+            runningSum = 0
+            
+        }
+        
+    } // End of playButton
+    
+    @IBAction func addButton(sender: UIButton) {
+        
+        let newSum = runningSum + userInput
+        updateLbl(runningSum, mul: userInput, newSum: newSum)
+        runningSum += userInput
+        
+        if isGameOver() {
+            restartGame()
+        }
+        
+    }
+    
+    // function to restart game
+    func restartGame() {
+        
+        userInput = 0
+        textField.text = ""
+        
+        multipleLogo.hidden = false
+        playButton.hidden = false
+        textField.hidden = false
+        addButton.hidden = true
+        pressToAdd.hidden = true
+    }
+    
+    // function to determine is game is over
+    func isGameOver() -> Bool {
+        
+        if runningSum >= MAXLIMIT {
+            return true
+        } else {
+            return false
+        }
+        
+    } // End of isGameOver
+    
+    func resetLbl() {
+        pressToAdd.text = "Press Add to add!"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateLbl(oldSum: Int, mul: Int, newSum: Int) {
+        pressToAdd.text = "\(oldSum) + \(mul) = \(newSum)"
     }
 
+    
 
 }
 
